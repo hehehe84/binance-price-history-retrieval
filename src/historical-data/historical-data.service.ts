4f5054ad60807d847115e9b8bc3eb510036f5664
hehehe84 <antoine.picott@gmail.com>
2025-08-21 12:01:58 +0200

    to simplify our approach and usage of our endpoint, we are initializing a dto to use in our service to retrieve historical data

--- src/historical-data/historical-data.service.ts
+++ src/historical-data/historical-data.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Kline, KlineDocument } from './schema/historical-data.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class HistoricalDataService {
   //API URL used to retrieve data:
   BINANCE_BASE_URL = 'https://api.binance.com/api/v3';
+
+  constructor(
+    @InjectModel(Kline.name) private klineModel: Model<KlineDocument>,
+  ) {}
+
+  async getKlines(params):
 }
