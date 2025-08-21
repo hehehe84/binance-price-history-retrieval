8ab70d69b047fdcc4a0e8a20b2e678decea08e7a
hehehe84 <antoine.picott@gmail.com>
2025-08-21 12:20:57 +0200

    creation of a basic historical-data service to get the Klines

--- src/historical-data/historical-data.service.ts
+++ src/historical-data/historical-data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Kline, KlineDocument } from './schema/historical-data.schema';
 import { Model } from 'mongoose';
+import { KlinesDto } from './dto/klines.dto';
+import axios from 'axios';
 
 @Injectable()
 export class HistoricalDataService {
@@ -12,5 +14,28 @@ export class HistoricalDataService {
     @InjectModel(Kline.name) private klineModel: Model<KlineDocument>,
   ) {}
 
-  async getKlines(params):
+  async getKlines(params: KlinesDto): Promise<any[]> {
+    try {
+      const queryParams = new URLSearchParams({
+        symbol: params.symbol,
+        interval: params.interval,
+        ...(params.startTime && { startTime: params.startTime.toString() }),
+        ...(params.limit && { limit: params.limit.toString() }),
+      });
+
+      const response = await axios.get(
+        `${this.BINANCE_BASE_URL}/klines?${queryParams}`,
+      );
+
+      // Saving klines inside mongoDB database - creation of a method in this service
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return response.data;
+    } catch (error) {
+      throw new Error(
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        `Failed to fetch klines: ${error.response?.data?.msg || error.message}`,
+      );
+    }
+  }
 }
